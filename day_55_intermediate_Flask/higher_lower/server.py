e3f758a2bf8e43061c6ec851598c6d3c0327d594
michael <michael.collazos@gmail.com>
2024-08-05 21:37:21 -0400

    updated strings

--- day_55_intermediate_Flask/higher_lower/server.py
+++ day_55_intermediate_Flask/higher_lower/server.py
@@ -20,13 +20,13 @@ random_num = random.randint(0, 9)
 @app.route("/<int:number>")
 def greet(number):
     if random_num == number:
-        return f"<h1>You Found me</h1>" \
+        return "<h1>You Found me</h1>" \
         "<img src='https://media.giphy.com/media/4T7e4DmcrP9du/giphy.gif'/>"
     elif random_num < number:
-        return f"<h1> Too high, try again! </h1>" \
+        return "<h1> Too high, try again! </h1>" \
         "<img src='https://media.giphy.com/media/3o6ZtaO9BZHcOjmErm/giphy.gif'/>"
     elif random_num > number:
-        return f"<h1> Too low, try again! </h1>" \
+        return "<h1> Too low, try again! </h1>" \
         "<img src='https://media.giphy.com/media/jD4DwBtqPXRXa/giphy.gif'/>"
         
 
